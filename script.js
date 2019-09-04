7daf0a3e320a454eb28e954cc234e2543083f5ad
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2019-09-04 13:10:59 +0200

    Add hero content change coordinated with nav

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 window.onload = function() {
-
-	// Background color change for navigation
+	
+	// Hero content & background color change (coordination with navigation)
+	var main_hero = document.getElementById("main_hero");
+	var tales = document.getElementById("hero_1");
+	var games = document.getElementById("hero_2");
+	var drawings = document.getElementById("hero_3");
+	var shop = document.getElementById("hero_4");
 	var hero = document.getElementById('hero');
 	var nav_colors = document.getElementsByClassName("nav_color");
 
 	for (var i = 0; i < nav_colors.length; i++) {
 		nav_colors[i].addEventListener('mouseenter', function () {
 			hero.style.backgroundColor = window.getComputedStyle(this).backgroundColor;
+			main_hero.style.display = 'none';
+			switch (this.dataset.name) {
+				case 'tales':
+					tales.style.display = 'flex';
+					break;
+				case 'games':
+					games.style.display = 'flex';
+					break;
+				case 'drawings':
+					drawings.style.display = 'flex';
+					break;
+				case 'shop':
+					shop.style.display = 'flex';
+					break;
+				default:
+					main_hero.style.display = 'flex';
+			}
 		});
 		nav_colors[i].addEventListener('mouseleave', function () {
 			hero.style.backgroundColor = "#9ed2cf";
+			main_hero.style.display = 'flex';
+			tales.style.display = 'none';
+			games.style.display = 'none';
+			drawings.style.display = 'none';
+			shop.style.display = 'none';
 		});
 	}
 
@@ -25,6 +52,16 @@ window.onload = function() {
 		$('.open').removeClass('openned');
 		event.stopPropagation();
 	});
-		
+
+	// Hero content change
+	/*var fairy_tales = $('.fairy_tales');
+	fairy_tales.on('mouseenter', function() {
+		$('.main_hero').css('display', 'none');
+		$('.fairy_tales_hero').css('display', 'flex');
+	});
+	fairy_tales.on('mouseleave', function() {
+		$('.fairy_tales_hero').css('display', 'none');
+		$('.main_hero').css('display', 'flex');
+	});*/
 }
 
